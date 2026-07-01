74c12fe78069db9ce9b42875f9417f8a776e8d73
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-07-01 22:23:21 +0300

    Описаны методы makeCoffee, calculateCups и printCoffeeName в классе CoffeeMachine

--- src/main/java/ru/alfabank/edu/day11/CoffeeMachine.java
+++ src/main/java/ru/alfabank/edu/day11/CoffeeMachine.java
@@ -1,4 +1,25 @@
 package ru.alfabank.edu.day11;
 
 public class CoffeeMachine {
+
+    // метод приготовления кофе
+    public void makeCoffee(int amountWater) {
+        if (amountWater <= 200) {
+            throw new NotEnoughWaterException("Закончилась вода, долейте воду в резервуар для воды!!!!");
+        }
+        System.out.println("Кофе готов!");
+    }
+
+    //метод подсчета, сколько чашек кофе можно приготовить
+    public int calculateCups(int amountMl, int sizeCup) {
+        return amountMl / sizeCup;
+    }
+
+    //метод вывод в консоль названия кофе
+    public void printCoffeeName(String nameCoffee) {
+        if (nameCoffee == null) {
+            throw new NullPointerException("Выберете кофе!!!");
+        }
+        System.out.println(nameCoffee.toUpperCase());
+    }
 }
